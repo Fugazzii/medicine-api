9aca1d99312fb93b1b2114ecdeea0e32166c05e0
Fugazzii <sichinavailia@gmail.com>
2023-10-07 19:21:05 +0400

    Integrated joi pipe

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { ClientModule } from "./client.module";
-import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(ClientModule);
