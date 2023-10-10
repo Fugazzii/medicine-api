d2d7bca3c2b9c5a7da8bdb24a469aa3bf6e6f95b
Fugazzii <sichinavailia@gmail.com>
2023-10-10 10:39:34 +0400

    Filled cities and countries with example data

---
+++ sql/cities.sql
@@ -0,0 +1,143 @@
+-- United States
+INSERT INTO cities ("countryId", name)
+VALUES
+    (1, 'New York'),
+    (1, 'Los Angeles'),
+    (1, 'Chicago'),
+    (1, 'Houston'),
+    (1, 'Miami');
+
+-- Canada
+INSERT INTO cities ("countryId", name)
+VALUES
+    (2, 'Toronto'),
+    (2, 'Vancouver'),
+    (2, 'Montreal'),
+    (2, 'Calgary'),
+    (2, 'Ottawa');
+
+-- United Kingdom
+INSERT INTO cities ("countryId", name)
+VALUES
+    (3, 'London'),
+    (3, 'Manchester'),
+    (3, 'Birmingham'),
+    (3, 'Glasgow'),
+    (3, 'Liverpool');
+
+-- Australia
+INSERT INTO cities ("countryId", name)
+VALUES
+    (4, 'Sydney'),
+    (4, 'Melbourne'),
+    (4, 'Brisbane'),
+    (4, 'Perth'),
+    (4, 'Adelaide');
+
+-- Germany
+INSERT INTO cities ("countryId", name)
+VALUES
+    (5, 'Berlin'),
+    (5, 'Munich'),
+    (5, 'Hamburg'),
+    (5, 'Frankfurt'),
+    (5, 'Cologne');
+
+-- France
+INSERT INTO cities ("countryId", name)
+VALUES
+    (6, 'Paris'),
+    (6, 'Marseille'),
+    (6, 'Lyon'),
+    (6, 'Toulouse'),
+    (6, 'Nice');
+
+-- Japan
+INSERT INTO cities ("countryId", name)
+VALUES
+    (7, 'Tokyo'),
+    (7, 'Osaka'),
+    (7, 'Kyoto'),
+    (7, 'Yokohama'),
+    (7, 'Sapporo');
+
+-- India
+INSERT INTO cities ("countryId", name)
+VALUES
+    (8, 'Mumbai'),
+    (8, 'Delhi'),
+    (8, 'Bangalore'),
+    (8, 'Kolkata'),
+    (8, 'Chennai');
+
+-- Brazil
+INSERT INTO cities ("countryId", name)
+VALUES
+    (9, 'Sao Paulo'),
+    (9, 'Rio de Janeiro'),
+    (9, 'Brasilia'),
+    (9, 'Salvador'),
+    (9, 'Fortaleza');
+
+-- China
+INSERT INTO cities ("countryId", name)
+VALUES
+    (10, 'Beijing'),
+    (10, 'Shanghai'),
+    (10, 'Guangzhou'),
+    (10, 'Shenzhen'),
+    (10, 'Chengdu');
+
+-- Russia
+INSERT INTO cities ("countryId", name)
+VALUES
+    (11, 'Moscow'),
+    (11, 'Saint Petersburg'),
+    (11, 'Novosibirsk'),
+    (11, 'Yekaterinburg'),
+    (11, 'Kazan');
+
+-- South Africa
+INSERT INTO cities ("countryId", name)
+VALUES
+    (12, 'Johannesburg'),
+    (12, 'Cape Town'),
+    (12, 'Durban'),
+    (12, 'Pretoria'),
+    (12, 'Bloemfontein');
+
+-- Mexico
+INSERT INTO cities ("countryId", name)
+VALUES
+    (13, 'Mexico City'),
+    (13, 'Guadalajara'),
+    (13, 'Monterrey'),
+    (13, 'Puebla'),
+    (13, 'Tijuana');
+
+-- Argentina
+INSERT INTO cities ("countryId", name)
+VALUES
+    (14, 'Buenos Aires'),
+    (14, 'Cordoba'),
+    (14, 'Rosario'),
+    (14, 'Mendoza'),
+    (14, 'San Miguel de Tucuman');
+
+-- Sweden
+INSERT INTO cities ("countryId", name)
+VALUES
+    (15, 'Stockholm'),
+    (15, 'Gothenburg'),
+    (15, 'Malmö'),
+    (15, 'Uppsala'),
+    (15, 'Västerås');
+
+-- Georgia
+INSERT INTO cities ("countryId", name)
+VALUES
+    (16, 'Tbilisi'),
+    (16, 'Kutaisi'),
+    (16, 'Batumi'),
+    (16, 'Zugdidi'),
+    (16, 'Sukhumi');
---
+++ sql/countries.sql
@@ -0,0 +1,18 @@
+INSERT INTO countries (name)
+VALUES
+    ('United States'),
+    ('Canada'),
+    ('United Kingdom'),
+    ('Australia'),
+    ('Germany'),
+    ('France'),
+    ('Japan'),
+    ('India'),
+    ('Brazil'),
+    ('China'),
+    ('Russia'),
+    ('South Africa'),
+    ('Mexico'),
+    ('Argentina'),
+    ('Sweden'), 
+    ('Georgia');
