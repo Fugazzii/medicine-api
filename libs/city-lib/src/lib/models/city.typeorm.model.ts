ae3ed875b74832f804857b768e8fc2830513ad38
Fugazzii <sichinavailia@gmail.com>
2023-10-10 15:03:22 +0400

    Added coordinated to cities table and filled with updated data

--- libs/city-lib/src/lib/models/city.typeorm.model.ts
+++ libs/city-lib/src/lib/models/city.typeorm.model.ts
@@ -1,15 +1,21 @@
 import { CityEntity } from "../entities";
 import { Column, ManyToOne, Entity as Model, PrimaryGeneratedColumn } from "typeorm";
-import { CountryEntity } from "@app/country-lib";
+import { CountryEntity, CountryTypeormModel } from "@app/country-lib";
 
 @Model("cities")
 export class CityTypeormModel implements CityEntity {
     @PrimaryGeneratedColumn()
     public id: number;
 
-    @ManyToOne(() => CityTypeormModel, (country: CountryEntity) => country.id)
+    @ManyToOne(() => CountryTypeormModel, (country: CountryEntity) => country.id)
     public country: number;
 
     @Column({ type: "varchar", length: 256 })
     public name: string;
+
+    @Column({ type: "float" })
+    public longtitude: number;
+
+    @Column({ type: "float" })
+    public latitude: number;
 }
--- sql/cities.sql
+++ sql/cities.sql
@@ -1,143 +1,143 @@
 -- United States
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (1, 'New York'),
-    (1, 'Los Angeles'),
-    (1, 'Chicago'),
-    (1, 'Houston'),
-    (1, 'Miami');
+    (1, 'New York', 40.7128, -74.0060),
+    (1, 'Los Angeles', 34.0522, -118.2437),
+    (1, 'Chicago', 41.8781, -87.6298),
+    (1, 'Houston', 29.7604, -95.3698),
+    (1, 'Miami', 25.7617, -80.1918);
 
 -- Canada
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (2, 'Toronto'),
-    (2, 'Vancouver'),
-    (2, 'Montreal'),
-    (2, 'Calgary'),
-    (2, 'Ottawa');
+    (2, 'Toronto', 43.651070, -79.347015),
+    (2, 'Vancouver', 49.2827, -123.1207),
+    (2, 'Montreal', 45.5017, -73.5673),
+    (2, 'Calgary', 51.0447, -114.0719),
+    (2, 'Ottawa', 45.4215, -75.6981);
 
 -- United Kingdom
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (3, 'London'),
-    (3, 'Manchester'),
-    (3, 'Birmingham'),
-    (3, 'Glasgow'),
-    (3, 'Liverpool');
+    (3, 'London', 51.5074, -0.1278),
+    (3, 'Manchester', 53.4839, -2.2446),
+    (3, 'Birmingham', 52.4862, -1.8904),
+    (3, 'Glasgow', 55.8652, -4.2576),
+    (3, 'Liverpool', 53.4084, -2.9916);
 
 -- Australia
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (4, 'Sydney'),
-    (4, 'Melbourne'),
-    (4, 'Brisbane'),
-    (4, 'Perth'),
-    (4, 'Adelaide');
+    (4, 'Sydney', -33.8651, 151.2099),
+    (4, 'Melbourne', -37.8136, 144.9631),
+    (4, 'Brisbane', -27.4698, 153.0251),
+    (4, 'Perth', -31.9505, 115.8605),
+    (4, 'Adelaide', -34.9285, 138.6007);
 
 -- Germany
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (5, 'Berlin'),
-    (5, 'Munich'),
-    (5, 'Hamburg'),
-    (5, 'Frankfurt'),
-    (5, 'Cologne');
+    (5, 'Berlin', 52.5200, 13.4050),
+    (5, 'Munich', 48.1351, 11.5820),
+    (5, 'Hamburg', 53.5511, 9.9937),
+    (5, 'Frankfurt', 50.1109, 8.6821),
+    (5, 'Cologne', 50.9375, 6.9603);
 
 -- France
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (6, 'Paris'),
-    (6, 'Marseille'),
-    (6, 'Lyon'),
-    (6, 'Toulouse'),
-    (6, 'Nice');
+    (6, 'Paris', 48.8566, 2.3522),
+    (6, 'Marseille', 43.2965, 5.3698),
+    (6, 'Lyon', 45.75, 4.85),
+    (6, 'Toulouse', 43.6047, 1.4442),
+    (6, 'Nice', 43.7102, 7.2620);
 
 -- Japan
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (7, 'Tokyo'),
-    (7, 'Osaka'),
-    (7, 'Kyoto'),
-    (7, 'Yokohama'),
-    (7, 'Sapporo');
+    (7, 'Tokyo', 35.682839, 139.759455),
+    (7, 'Osaka', 34.6937, 135.5023),
+    (7, 'Kyoto', 35.0116, 135.7681),
+    (7, 'Yokohama', 35.4437, 139.6380),
+    (7, 'Sapporo', 43.0621, 141.3544);
 
 -- India
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (8, 'Mumbai'),
-    (8, 'Delhi'),
-    (8, 'Bangalore'),
-    (8, 'Kolkata'),
-    (8, 'Chennai');
+    (8, 'Mumbai', 19.0760, 72.8777),
+    (8, 'Delhi', 28.6139, 77.2090),
+    (8, 'Bangalore', 12.9716, 77.5946),
+    (8, 'Kolkata', 22.5726, 88.3639),
+    (8, 'Chennai', 13.0827, 80.2707);
 
 -- Brazil
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (9, 'Sao Paulo'),
-    (9, 'Rio de Janeiro'),
-    (9, 'Brasilia'),
-    (9, 'Salvador'),
-    (9, 'Fortaleza');
+    (9, 'Sao Paulo', -23.5505, -46.6333),
+    (9, 'Rio de Janeiro', -22.9068, -43.1729),
+    (9, 'Brasilia', -15.7801, -47.9292),
+    (9, 'Salvador', -12.9716, -38.5014),
+    (9, 'Fortaleza', -3.7172, -38.5433);
 
 -- China
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (10, 'Beijing'),
-    (10, 'Shanghai'),
-    (10, 'Guangzhou'),
-    (10, 'Shenzhen'),
-    (10, 'Chengdu');
+    (10, 'Beijing', 39.9042, 116.4074),
+    (10, 'Shanghai', 31.2304, 121.4737),
+    (10, 'Guangzhou', 23.1291, 113.2644),
+    (10, 'Shenzhen', 22.5431, 114.0579),
+    (10, 'Chengdu', 30.5728, 104.066);
 
 -- Russia
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (11, 'Moscow'),
-    (11, 'Saint Petersburg'),
-    (11, 'Novosibirsk'),
-    (11, 'Yekaterinburg'),
-    (11, 'Kazan');
+    (11, 'Moscow', 55.7558, 37.6176),
+    (11, 'Saint Petersburg', 59.9343, 30.3351),
+    (11, 'Novosibirsk', 55.0084, 82.9357),
+    (11, 'Yekaterinburg', 56.8389, 60.6057),
+    (11, 'Kazan', 55.7961, 49.1064);
 
 -- South Africa
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (12, 'Johannesburg'),
-    (12, 'Cape Town'),
-    (12, 'Durban'),
-    (12, 'Pretoria'),
-    (12, 'Bloemfontein');
+    (12, 'Johannesburg', -26.2041, 28.0473),
+    (12, 'Cape Town', -33.9249, 18.4241),
+    (12, 'Durban', -29.8587, 31.0218),
+    (12, 'Pretoria', -25.7461, 28.1881),
+    (12, 'Bloemfontein', -29.0852, 26.1596);
 
 -- Mexico
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (13, 'Mexico City'),
-    (13, 'Guadalajara'),
-    (13, 'Monterrey'),
-    (13, 'Puebla'),
-    (13, 'Tijuana');
+    (13, 'Mexico City', 19.4326, -99.1332),
+    (13, 'Guadalajara', 20.6597, -103.3496),
+    (13, 'Monterrey', 25.6866, -100.3161),
+    (13, 'Puebla', 19.0402, -98.2062),
+    (13, 'Tijuana', 32.5149, -117.0382);
 
 -- Argentina
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (14, 'Buenos Aires'),
-    (14, 'Cordoba'),
-    (14, 'Rosario'),
-    (14, 'Mendoza'),
-    (14, 'San Miguel de Tucuman');
+    (14, 'Buenos Aires', -34.6118, -58.4173),
+    (14, 'Cordoba', -31.4201, -64.1888),
+    (14, 'Rosario', -32.9442, -60.6505),
+    (14, 'Mendoza', -32.8902, -68.8441),
+    (14, 'San Miguel de Tucuman', -26.8241, -65.2226);
 
 -- Sweden
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (15, 'Stockholm'),
-    (15, 'Gothenburg'),
-    (15, 'Malmö'),
-    (15, 'Uppsala'),
-    (15, 'Västerås');
+    (15, 'Stockholm', 59.3293, 18.0686),
+    (15, 'Gothenburg', 57.7089, 11.9746),
+    (15, 'Malmö', 55.6044, 13.0038),
+    (15, 'Uppsala', 59.8586, 17.6389),
+    (15, 'Västerås', 59.6093, 16.5456);
 
 -- Georgia
-INSERT INTO cities ("countryId", name)
+INSERT INTO cities ("countryId", name, longtitude, latitude)
 VALUES
-    (16, 'Tbilisi'),
-    (16, 'Kutaisi'),
-    (16, 'Batumi'),
-    (16, 'Zugdidi'),
-    (16, 'Sukhumi');
+    (16, 'Tbilisi', 41.7151, 44.8271),
+    (16, 'Kutaisi', 42.2679, 42.6946),
+    (16, 'Batumi', 41.6432, 41.6397),
+    (16, 'Zugdidi', 42.5083, 41.8698),
+    (16, 'Sukhumi', 43.0035, 41.0234);
